cbb879903a68bac80029e53b78d11f59e07a41e8
Thomas Massmann <thomas@propertyshelf.com>
2014-02-02 10:32:07 +0100

    Added intitialize for Zope2 product.

--- src/theming/toolkit/views/__init__.py
+++ src/theming/toolkit/views/__init__.py
@@ -1,1 +1,6 @@
-"""A product that provides a bunch of usefull views"""
+# -*- coding: utf-8 -*-
+"""Custom views for Plone Add-Ons."""
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
